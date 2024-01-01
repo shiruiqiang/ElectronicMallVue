e404491535193ebc88b3e645fe463c4128510c06
shiruiqiang <535285745@qq.com>
2024-01-02 00:53:54 +0800

    first commit

---
+++ src/components/bottom.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <p>
+      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=14011002000251" target="_blank" rel="noopener noreferrer">
+        晋公网安备14011002000251号
+      </a>
+      <span class="mod_copyright_split">|</span>
+      <a href="http://beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">
+        晋ICP备2023015085号
+      </a>
+      <span class="mod_copyright_split">|</span>
+      <span>服务热线：19103465638</span>
+      <span class="mod_copyright_split">|</span>
+      <span>太原简爱聚鑫科技有限责任公司</span>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex; /* 设置为flex布局 */
+  justify-content: center; /* 水平居中 */
+  font: 12px/1.5 Microsoft YaHei,Heiti SC,tahoma,arial,Hiragino Sans GB,"\5B8B\4F53",sans-serif;
+}
+a {
+  color: #999;
+  text-decoration: none;
+}
+span{
+  color: #999;
+}
+.mod_copyright_split {
+  margin: 0 7px;
+  color: #ccc;
+}
+
+
+
+
+</style>
